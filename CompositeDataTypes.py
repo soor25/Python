20297e481a433fadd63d2f646d72cbd881c9bd81
soor25 <44835492+soor25@users.noreply.github.com>
2023-09-08 10:12:48 -0400

    Variables

---
+++ CompositeDataTypes.py
@@ -0,0 +1,4 @@
+# list, tuple, dictionary, frozenset, set
+
+our_list = ['a','b','c', 1.5]
+print(our_list)
--- Variables&DataTypes.py
+++ Variables&DataTypes.py
@@ -1,11 +1,15 @@
 # Variables in Python need to be descriptive
-
 age = 1
 print(type(age))
+
+# You can do the same with string() interger() and float()
+name = 'Mansur'
+print(type(name))
 
 # We want this to return as a float not a int so we will use cast
 number = float(1)
 print(type(number))
 
-number_and_age = (number, age)
+# Practice multi word variables seperating with a _ underscore
+number_and_age = (name,number, age)
 print(number_and_age)
