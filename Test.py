4948724b54b67eb0baacbb0c99c7413cc968bdc0
soor25 <44835492+soor25@users.noreply.github.com>
2023-09-08 09:37:19 -0400

    Initial commit

---
+++ Test.py
@@ -0,0 +1,13 @@
+# This is my first Python Script
+# This is a comment!
+
+print('Hello World!')
+
+# Python uses whitespaces
+
+x = 1
+if x < 0:
+    print('x is greater than 0')
+# Notice else is its own block of code its not indented it does it itself when you hit enter
+else:
+    print('x is less than 0')
---
+++ Variables&DataTypes.py
@@ -0,0 +1,11 @@
+# Variables in Python need to be descriptive
+
+age = 1
+print(type(age))
+
+# We want this to return as a float not a int so we will use cast
+number = float(1)
+print(type(number))
+
+number_and_age = (number, age)
+print(number_and_age)
